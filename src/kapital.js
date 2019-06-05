192e6c648306413705a31d5bdd1aa13999c0f28b
maxim <maximum454@gmail.com>
2019-06-05 17:07:33 +0600

    fix

--- src/kapital.js
+++ src/kapital.js
@@ -1,4 +1,5 @@
 import './kapital.scss'
+
 import './img/logo.png'
 import './img/logo.svg'
 import './img/Rectangle2.1.png'
@@ -13,3 +14,40 @@ import './img/c2.jpg'
 import './img/current-graf.svg'
 import './img/search.svg'
 
+
+/*
+function responseMenu() {
+    $('navbar__dropdown .nav-item').appendTo('.navbar-nav');
+    var items = $('.navbar-nav .nav-item');
+    var max_width = $('.navbar-nav').width() - $('.navbar__dd').outerWidth();
+    var width = 0;
+    var hide_from = 0;
+    console.log('len'+items.length);
+    items.css({'width': 'auto'});
+    items.each(function (index) {
+        if (width + $(this).outerWidth() > max_width) {
+            return false;
+        } else {
+            hide_from = index;
+            width += $(this).outerWidth();
+        }
+    });
+    if (hide_from < items.length - 1) {
+        items.eq(hide_from).nextAll('.nav-item').appendTo('.navbar__dropdown');
+        items.css({'width': (max_width / (hide_from + 1)) + 'px'});
+        $('.navbar__dd').show();
+    } else {
+        $('.navbar__dd').hide();
+    }
+}
+
+
+$(function () {
+    $('.navbar').on('click', '.navbar__toggle', function () {
+        $('.navbar__dropdown').toggle();
+    });
+
+    $(window).on('resize', function(){
+        responseMenu();
+    }).trigger('resize');
+});*/
